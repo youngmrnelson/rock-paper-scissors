d0ec1045cce9929b646a12d31e66d06b898ab969
Daniel Nelson <rockoman.exe@gmail.com>
2025-01-16 17:59:05 -0500

    add: create function to evaluate each round

--- script.js
+++ script.js
@@ -36,6 +36,33 @@ function displayLosingMessage() {
   console.log("Computer wins!");
 }
 
+function displayDrawMessage() {
+  console.log("It's a draw!");
+}
+
 function displayPlayerChoices(a, b) {
   console.log(`Human: ${a}\nComputer: ${b}`);
 }
+
+function evaluateRound(a, b) {
+  displayPlayerChoices(a, b);
+  if (a === "rock" && b === "scissors") {
+    increasePlayerScore();
+  } else if (a === "rock" && b === "paper") {
+    increaseComputerScore();
+  }
+  if (a === "paper" && b === "rock") {
+    increasePlayerScore();
+  } else if (a === "paper" && b === "scissors") {
+    increaseComputerScore();
+  }
+  if (a === "scissors" && b === "paper") {
+    increasePlayerScore();
+  } else if (a === "scissors" && b === "rock") {
+    increaseComputerScore();
+  } else {
+    displayDrawMessage();
+  }
+}
+
+playRound();
