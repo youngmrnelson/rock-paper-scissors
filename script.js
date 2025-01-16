69eee04eb0934af62eb8d30972ef98b28329d449
Daniel Nelson <rockoman.exe@gmail.com>
2025-01-16 17:39:32 -0500

    add: create  getHumanChoice function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 "use strict";
 
 const choices = ["rock", "paper", "scissors"];
+const humanScore = 0;
+const computerScore = 0;
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  return prompt("Rock, Paper, Scissors?", "rock").toLowerCase().trim();
+}
